854810b89816b3308ccddf4945f1d191a9001594
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 22:41:51 +0100

    Adds sample data for testing

---
+++ src/db/data/test-data/issues/issue-images.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    issue_id: 1,
+    image_url: "https://images.pexels.com/photos/5712710/pexels-photo-5712710.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
+    issue_id: 1,
+    image_url: "https://images.pexels.com/photos/5768217/pexels-photo-5768217.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
+    issue_id: 3,
+    image_url: "https://images.pexels.com/photos/168292/pexels-photo-168292.jpeg?auto=compress&cs=tinysrgb&w=800"
+  }
+]
