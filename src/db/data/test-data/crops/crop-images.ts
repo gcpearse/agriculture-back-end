dd814a0651529c66bca182e32ed704643424aebd
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 19:52:33 +0100

    Updates sample data

--- src/db/data/test-data/crops/crop-images.ts
+++ src/db/data/test-data/crops/crop-images.ts
@@ -4,6 +4,10 @@ export default [
     image_url: "https://images.pexels.com/photos/143133/pexels-photo-143133.jpeg?auto=compress&cs=tinysrgb&w=800"
   },
   {
+    crop_id: 1,
+    image_url: "https://images.pexels.com/photos/1268101/pexels-photo-1268101.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
     crop_id: 2,
     image_url: "https://images.pexels.com/photos/693794/pexels-photo-693794.jpeg?auto=compress&cs=tinysrgb&w=800"
   },
