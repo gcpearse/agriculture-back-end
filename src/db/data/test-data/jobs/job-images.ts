5a3ea53f482d4fa8b0e1ee56e3ab903abd7b224f
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 23:03:09 +0100

    Adds sample data for testing

---
+++ src/db/data/test-data/jobs/job-images.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    job_id: 1,
+    image_url: "https://images.pexels.com/photos/175415/pexels-photo-175415.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
+    job_id: 1,
+    image_url: "https://images.pexels.com/photos/144206/pexels-photo-144206.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
+    job_id: 2,
+    image_url: "https://images.pexels.com/photos/12113255/pexels-photo-12113255.jpeg?auto=compress&cs=tinysrgb&w=800"
+  }
+]
