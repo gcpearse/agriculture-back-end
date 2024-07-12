d273b34ffb6f5c58ab66c36b84fe5e3bf9660f43
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 17:59:08 +0100

    Adds sample crop data for testing

---
+++ src/db/data/test-data/crops/crops.ts
@@ -0,0 +1,34 @@
+export default [
+  {
+    plot_id: 1,
+    name: "carrot",
+    variety: null,
+    quantity: 20,
+    date_planted: new Date("2024-06-20"),
+    harvest_date: new Date("2024-09-15")
+  },
+  {
+    plot_id: 1,
+    name: "apple",
+    variety: "lord derby",
+    quantity: 1,
+    date_planted: new Date("2023-09-21"),
+    harvest_date: new Date("2024-09-21")
+  },
+  {
+    plot_id: 2,
+    name: "peach",
+    variety: null,
+    quantity: 2,
+    date_planted: new Date("2020-06-30"),
+    harvest_date: new Date("2024-07-25")
+  },
+  {
+    plot_id: 2,
+    name: "blackcurrant",
+    variety: null,
+    quantity: null,
+    date_planted: new Date("2022-08-30"),
+    harvest_date: new Date("2024-07-14")
+  }
+]
