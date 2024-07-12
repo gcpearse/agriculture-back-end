44dbafc527259c7c97c46fcff463994643dfc7c2
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 22:56:47 +0100

    Updates custom types

--- src/types.ts
+++ src/types.ts
@@ -39,6 +39,20 @@ export type Issue = {
 }
 
 
+export type Job = {
+  job_id?: number
+  plot_id: number
+  crop_id: number | null
+  issue_id: number | null
+  title: string
+  description: string
+  date_added: Date
+  deadline: Date | null
+  is_started: boolean
+  is_completed: boolean
+}
+
+
 type Comment = {
   comment_id?: number
   body: string
