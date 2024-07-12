96c2c3fc7fea1b04e84d93968a69207e7209365d
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 19:38:05 +0100

    Updates sample data - fixes timestamp bug

--- src/db/data/test-data/crops/crop-comments.ts
+++ src/db/data/test-data/crops/crop-comments.ts
@@ -2,21 +2,21 @@ export default [
   {
     crop_id: 1,
     body: "These will need more water in future.",
-    created_at: 1720267245
+    created_at: "2024-07-01 12:05:59.95996"
   },
   {
     crop_id: 1,
     body: "Slugs are eating the leaves.",
-    created_at: 1720537482
+    created_at: "2024-06-30 14:23:01.38919"
   },
   {
     crop_id: 3,
     body: "They look bigger than last year's batch.",
-    created_at: 1719832599
+    created_at: "2023-06-28 17:12:09.872256"
   },
   {
     crop_id: 4,
     body: "Too many for one person to harvest.",
-    created_at: 1720775087
-  },
+    created_at: "2024-07-12 09:33:12.232434"
+  }
 ]
