617886e9f1fac3e8f4f55d27196c3aedd25eed21
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 19:37:35 +0100

    Updates index

--- src/db/data/test-data/index.ts
+++ src/db/data/test-data/index.ts
@@ -2,11 +2,13 @@ import userData from "./users/users"
 import plotData from "./plots/plots"
 import plotImageData from "./plots/plot-images"
 import cropData from "./crops/crops"
+import cropCommentData from "./crops/crop-comments"
 
 
 export default {
   userData,
   plotData,
   plotImageData,
-  cropData
+  cropData,
+  cropCommentData
 }
