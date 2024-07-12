0a9fc7d2df0600c58aa8b116789f66e4ba408dac
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 22:19:27 +0100

    Adds sample data for testing

---
+++ src/db/data/test-data/issues/issues.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    plot_id: 1,
+    title: "Broken gate",
+    description: "The gate has fallen off its hinges",
+    is_resolved: false
+  },
+  {
+    plot_id: 1,
+    title: "Weeds",
+    description: "The garden has become infested with weeds",
+    is_resolved: true
+  },
+  {
+    plot_id: 2,
+    title: "Rotten fence posts",
+    description: "Some of the fence posts are rotting and need replacing",
+    is_resolved: false
+  }
+]
