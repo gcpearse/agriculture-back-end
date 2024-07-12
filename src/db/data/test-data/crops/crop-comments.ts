7b7a119a83ff241246c62fbeb498ae176ef8bc81
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 18:19:08 +0100

    Adds sample crop comments for testing

---
+++ src/db/data/test-data/crops/crop-comments.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    crop_id: 1,
+    body: "These will need more water in future.",
+    created_at: 1720267245
+  },
+  {
+    crop_id: 1,
+    body: "Slugs are eating the leaves.",
+    created_at: 1720537482
+  },
+  {
+    crop_id: 3,
+    body: "They look bigger than last year's batch.",
+    created_at: 1719832599
+  },
+  {
+    crop_id: 4,
+    body: "Too many for one person to harvest.",
+    created_at: 1720775087
+  },
+]
