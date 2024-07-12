13a3d4552bd6a6179b9cb0ae0219d85537c26153
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 17:21:29 +0100

    Adds sample plot data for tests

---
+++ src/db/data/test-data/plots/plot-images.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    plot_id: 1,
+    image_url: "https://images.pexels.com/photos/226407/pexels-photo-226407.jpeg?auto=compress&cs=tinysrgb&w=800"
+  },
+  {
+    plot_id: 2,
+    image_url: "https://images.pexels.com/photos/574919/pexels-photo-574919.jpeg?auto=compress&cs=tinysrgb&w=800"
+  }
+]
---
+++ src/db/data/test-data/plots/plots.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    owner_id: 1,
+    name: "John's Garden",
+    type: "garden",
+    description: "A vegetable garden",
+    location: "Farmville",
+    area: 100
+  },
+  {
+    owner_id: 2,
+    name: "Olivia's Field",
+    type: "field",
+    description: "An orchard",
+    location: "Lemongrove",
+    area: 500
+  }
+]
