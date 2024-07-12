c093a7b16aa4f6f93651a84588acaedc63d6889c
gcpearse <glen.c.pearse@gmail.com>
2024-07-12 17:22:20 +0100

    Imports and exports user and plot data

---
+++ src/db/data/test-data/index.ts
@@ -0,0 +1,10 @@
+import userData from "./users/users"
+import plotData from "./plots/plots"
+import plotImageData from "./plots/plot-images"
+
+
+export default {
+  userData,
+  plotData,
+  plotImageData
+}
