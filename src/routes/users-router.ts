2c539e69bdf3f857d100620940db7e97d341ca79
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 19:29:35 +0100

    Adds route to patch user details

--- src/routes/users-router.ts
+++ src/routes/users-router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { verifyToken } from "../middleware/authentication"
-import { deleteUserByUsername, getUserByUsername } from "../controllers/user-controllers"
+import { deleteUserByUsername, getUserByUsername, patchUserByUsername } from "../controllers/user-controllers"
 
 
 export const usersRouter = Router()
 
 usersRouter.route("/:username")
   .get(verifyToken, getUserByUsername)
+  .patch(verifyToken, patchUserByUsername)
   .delete(verifyToken, deleteUserByUsername)
