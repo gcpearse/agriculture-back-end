dcc3bc147d9b4ab6c9d65e94bcbd6c53bbd22260
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 00:09:34 +0100

    Creates Swagger config options, orders middleware appropriately

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,45 @@
 import express from "express"
-import { postLogin, postRegistration } from "./controllers/auth-controllers"
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 import { handleCustomErrors, handleServerErrors } from "./middleware/errors"
 import { handleNotFound } from "./controllers/handle-not-found"
-import { verifyToken } from "./middleware/authentication"
-import { testAuth } from "./controllers/test-auth"
 import { usersRouter } from "./routes/users-router"
+import { authRouter } from "./routes/auth-router"
+import { loginRouter } from "./routes/login-router"
+import { registerRouter } from "./routes/register-router"
 
 
 export const app = express()
 
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Agriculture API",
+      description: "A full-stack application for gardeners, allotment owners, and other subsistence farmers to keep track of crops and jobs on their plots.",
+      version: "1.0.0"
+    }
+  },
+  apis: ["./dist/routes/*.js"]
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+
 app.use(express.json())
 
 
-app.post("/api/register", postRegistration)
+app.use("/api", registerRouter)
 
-app.post("/api/login", postLogin)
+app.use("/api", loginRouter)
 
-app.get("/api/auth", verifyToken, testAuth)
+app.use("/api", authRouter)
 
-app.use("/api/users", usersRouter)
+app.use("/api", usersRouter)
+
+
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 
 app.use(handleCustomErrors, handleServerErrors)
