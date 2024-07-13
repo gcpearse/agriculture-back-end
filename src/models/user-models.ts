c5b64bc91042ea644f04115453689620125c3c3d
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 14:15:21 +0100

    Adds logInUser function

--- src/models/user-models.ts
+++ src/models/user-models.ts
@@ -1,8 +1,8 @@
 import { db } from "../db"
-import { User } from "../types"
+import { Credentials, User } from "../types"
 
 
-export const insertUser = async ({ username, password, first_name, surname, uses_metric }: User) => {
+export const registerUser = async ({ username, password, first_name, surname, uses_metric }: User) => {
 
   const conflictCheck = await db.query(`
     SELECT username 
@@ -29,4 +29,39 @@ export const insertUser = async ({ username, password, first_name, surname, uses
   )
 
   return result.rows[0]
+}
+
+
+export const logInUser = async ({ username, password }: Credentials) => {
+
+  const usernameCheck = await db.query(`
+    SELECT username 
+    FROM users 
+    WHERE username = $1
+    `,
+    [username])
+
+  if (!usernameCheck.rowCount) {
+    return Promise.reject({
+      status: 404,
+      message: "Username not found"
+    })
+  }
+
+  const passwordCheck = await db.query(`
+    SELECT password
+    FROM users
+    WHERE username = $1
+    AND password = $2
+    `,
+    [username, password])
+
+  if (!passwordCheck.rowCount) {
+    return Promise.reject({
+      status: 401,
+      message: "Incorrect password"
+    })
+  }
+
+  return usernameCheck.rows[0]
 }
