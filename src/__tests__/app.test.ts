b2cc5040b39d13e5fdbf8276f6026020e33143a0
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 14:11:05 +0100

    Adds tests for POST requests to /api/login

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -3,7 +3,7 @@ import { db } from "../db"
 import { seed } from "../db/seeding/seed"
 import request from "supertest"
 import { app } from "../app"
-import { User } from "../types"
+import { Credentials, User } from "../types"
 
 
 beforeEach(() => seed(data))
@@ -52,5 +52,56 @@ describe("POST /api/register", () => {
       .expect(409)
 
     expect(body.message).toBe("Username already exists")
+  })
+})
+
+
+describe("POST /api/login", () => {
+
+  test("POST:200 Responds with the username of the logged in user when the credentials are valid", async () => {
+
+    const user: Credentials = {
+      username: "carrot_king",
+      password: "carrots123"
+    }
+
+    const { body } = await request(app)
+      .post("/api/login")
+      .send(user)
+      .expect(200)
+
+    expect(body).toMatchObject({
+      username: "carrot_king"
+    })
+  })
+
+  test("POST:404 Responds with an error message when the username is not found", async () => {
+
+    const user: Credentials = {
+      username: "mango_man",
+      password: "carrots123"
+    }
+
+    const { body } = await request(app)
+      .post("/api/login")
+      .send(user)
+      .expect(404)
+
+      expect(body.message).toBe("Username not found")
+  })
+
+  test("POST:401 Responds with an error message when the password is incorrect", async () => {
+
+    const user: Credentials = {
+      username: "carrot_king",
+      password: "apples123"
+    }
+
+    const { body } = await request(app)
+      .post("/api/login")
+      .send(user)
+      .expect(401)
+
+      expect(body.message).toBe("Incorrect password")
   })
 })
