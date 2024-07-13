8c6511a41fd8ec2162562da4e86f70938106b56e
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 19:31:23 +0100

    Adds tests for PATCH /api/users/:username

--- src/__tests__/users.test.ts
+++ src/__tests__/users.test.ts
@@ -55,6 +55,52 @@ describe("GET /api/users/:username", () => {
 })
 
 
+describe("PATCH /api/users/:username", () => {
+
+  test("PATCH:200 Responds with an updated user object", async () => {
+
+    const newDetails = {
+      username: "carrot_king",
+      first_name: "Johnny",
+      surname: "Smith-Jones",
+      uses_metric: true
+    }
+
+    const { body } = await request(app)
+      .patch("/api/users/carrot_king")
+      .send(newDetails)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+
+    expect(body.user).toMatchObject({
+      user_id: 1,
+      username: "carrot_king",
+      first_name: "Johnny",
+      surname: "Smith-Jones",
+      uses_metric: true
+    })
+  })
+
+  test("GET:403 Responds with a warning when the authenticated user attempts to edit another user's data", async () => {
+
+    const newDetails = {
+      username: "peach_princess",
+      first_name: "John",
+      surname: "Smith",
+      uses_metric: false
+    }
+
+    const { body } = await request(app)
+      .patch("/api/users/peach_princess")
+      .send(newDetails)
+      .set("Authorization", `Bearer ${token}`)
+      .expect(403)
+
+    expect(body.message).toBe("Permission to edit user data denied")
+  })
+})
+
+
 describe("DELETE /api/users/:username", () => {
 
   test("DELETE:204 Deletes the user with the given username", async () => {
