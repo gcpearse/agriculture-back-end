f6bfcd11cd0b7469c997a363cbaff4b5cb871ec1
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 18:30:09 +0100

    Adds selectUserByUsername model to retrieve user data from the db

---
+++ src/models/user-models.ts
@@ -0,0 +1,28 @@
+import { db } from "../db"
+
+
+export const selectUserByUsername = async (authorisedUser: string, username: string) => {
+
+  if (authorisedUser !== username) {
+    return Promise.reject({
+      status: 403,
+      message: "Access to user data denied"
+    })
+  }
+
+  const result = await db.query(`
+    SELECT user_id, username, first_name, surname, uses_metric
+    FROM users
+    WHERE username = $1
+    `,
+    [username])
+
+  if (!result.rowCount) {
+    return Promise.reject({
+      status: 404,
+      message: "User not found"
+    })
+  }
+    
+  return result.rows[0]
+}
