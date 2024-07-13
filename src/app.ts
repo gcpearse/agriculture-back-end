baa065576579c11b8d4486b3f0daee438690c929
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 14:13:56 +0100

    Updates imports

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { postLogin, postUser } from "./controllers/user-controllers"
+import { postLogin, postRegistration } from "./controllers/user-controllers"
 import { handleCustomErrors, handleServerErrors } from "./middleware/errors"
 import { handleNotFound } from "./controllers/handle-not-found"
 
@@ -9,7 +9,7 @@ export const app = express()
 
 app.use(express.json())
 
-app.post("/api/register", postUser)
+app.post("/api/register", postRegistration)
 app.post("/api/login", postLogin)
 
 app.use(handleCustomErrors, handleServerErrors)
--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express" 
-import { insertUser } from "../models/user-models"
+import { insertUser, logInUser } from "../models/user-models"
 
 
 export const postUser: RequestHandler = async (req, res, next) => {
@@ -7,6 +7,17 @@ export const postUser: RequestHandler = async (req, res, next) => {
   try {
     const user = await insertUser(req.body)
     res.status(201).send(user)
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+export const postLogin: RequestHandler = async (req, res, next) => {
+
+  try {
+    const username = await logInUser(req.body)
+    res.status(200).send(username)
   } catch (err) {
     next(err)
   }
