3ae8bb8a12499518923ae94947de33858280fbb7
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 16:16:15 +0100

    Adds authentication middleware

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,7 +1,35 @@
+import { RequestHandler } from "express"
 import jwt from "jsonwebtoken"
 
 
 export const generateToken = (username: { username: string }) => {
 
   return jwt.sign(username, process.env.JWT_SECRET!, { expiresIn: 3600 })
+}
+
+
+export const verifyToken: RequestHandler = (req, res, next) => {
+
+  const bearer = req.headers.authorization
+
+  const token = bearer && bearer.split(" ")[1]
+
+  if (!token) {
+    res.status(401).send({
+      message: "Unauthorised"
+    })
+  }
+
+  jwt.verify(token!, process.env.JWT_SECRET!, (err: any, user: any) => {
+
+    if (err) {
+      res.status(403).send({
+        message: "Forbidden"
+      })
+    }
+
+    req.body.user = user
+
+    next()
+  })
 }
