f19b476e9c32c7761d3aafca3c47754259bc5306
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 19:45:48 +0100

    Adds patchPasswordByUsername controller

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express"
-import { removeUserByUsername, selectUserByUsername, updateUserByUsername } from "../models/user-models"
+import { changedPasswordByUsername, removeUserByUsername, selectUserByUsername, updateUserByUsername } from "../models/user-models"
 
 
 export const getUserByUsername: RequestHandler = async (req, res, next) => {
@@ -25,6 +25,21 @@ export const patchUserByUsername: RequestHandler = async (req, res, next) => {
 
   try {
     const user = await updateUserByUsername(authorisedUser, username, req.body)
+    res.status(200).send({ user })
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+export const patchPasswordByUsername: RequestHandler = async (req, res, next) => {
+
+  const authorisedUser = req.body.user.username
+
+  const { username } = req.params
+
+  try {
+    const user = await changedPasswordByUsername(authorisedUser, username, req.body.password)
     res.status(200).send({ user })
   } catch (err) {
     next(err)
