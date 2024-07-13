cc36b341c50050155d44b00eeb5f97a366e547fa
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 16:15:35 +0100

    Adds route to /api/auth endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express"
 import { postLogin, postRegistration } from "./controllers/user-controllers"
 import { handleCustomErrors, handleServerErrors } from "./middleware/errors"
 import { handleNotFound } from "./controllers/handle-not-found"
+import { verifyToken } from "./middleware/authentication"
+import { testAuth } from "./controllers/test-auth"
 
 
 export const app = express()
@@ -11,6 +13,7 @@ app.use(express.json())
 
 app.post("/api/register", postRegistration)
 app.post("/api/login", postLogin)
+app.get("/api/auth", verifyToken, testAuth)
 
 app.use(handleCustomErrors, handleServerErrors)
 
