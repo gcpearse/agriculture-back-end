c4207777ccedb8554d32fcdeeb9b9c5b234e0652
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 11:52:49 +0100

    Adds middleware and /api/register endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
 import express from "express"
+import { postUser } from "./controllers/user-controllers"
+import { handleCustomErrors, handleServerErrors } from "./middleware/errors"
+import { handleNotFound } from "./controllers/handle-not-found"
+
 
 export const app = express()
+
+
+app.use(express.json())
+
+app.post("/api/register", postUser)
+
+app.use(handleCustomErrors, handleServerErrors)
+
+app.all("*", handleNotFound)
