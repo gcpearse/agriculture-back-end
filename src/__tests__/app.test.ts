272f2a52046bd515d28893f9e5680df5cd15f1b9
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 11:54:09 +0100

    Adds tests for POST requests to /api/register

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,7 +1,56 @@
 import data from "../db/data/test-data/index"
 import { db } from "../db"
 import { seed } from "../db/seeding/seed"
+import request from "supertest"
+import { app } from "../app"
+import { User } from "../types"
 
 
 beforeEach(() => seed(data))
 afterAll(() => db.end())
+
+
+describe("POST /api/register", () => {
+
+  test("POST:201 Responds with the newly created user", async () => {
+
+    const newUser: User = {
+      username: "farmer123",
+      password: "password123",
+      first_name: "Fred",
+      surname: "Flint",
+      uses_metric: true
+    }
+
+    const { body } = await request(app)
+      .post("/api/register")
+      .send(newUser)
+      .expect(201)
+
+    expect(body).toMatchObject({
+      username: "farmer123",
+      password: "password123",
+      first_name: "Fred",
+      surname: "Flint",
+      uses_metric: true
+    })
+  })
+
+  test("POST:409 Responds with an error message when the username already exists", async () => {
+
+    const newUser: User = {
+      username: "carrot_king",
+      password: "password234",
+      first_name: "Bob",
+      surname: "Booth",
+      uses_metric: false
+    }
+
+    const { body } = await request(app)
+      .post("/api/register")
+      .send(newUser)
+      .expect(409)
+
+    expect(body.message).toBe("Username already exists")
+  })
+})
