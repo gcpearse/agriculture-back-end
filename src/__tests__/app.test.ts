40c2d78c8f604196ad545d102d06d30fba852bbe
gcpearse <glen.c.pearse@gmail.com>
2024-07-13 16:16:37 +0100

    Adds tests for GET /api/auth to test authentication

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -103,3 +103,43 @@ describe("POST /api/login", () => {
     expect(body.message).toBe("Incorrect password")
   })
 })
+
+
+describe("GET /api/auth", () => {
+
+  test("GET:200 Responds with a success message when the credentials are valid", async () => {
+
+    const auth = await request(app)
+      .post("/api/login")
+      .send({
+        username: "carrot_king",
+        password: "carrots123",
+      })
+
+    const { body } = await request(app)
+      .get("/api/auth")
+      .set("Authorization", `Bearer ${auth.body.token}`)
+      .expect(200)
+
+    expect(body.message).toBe("Success")
+  })
+
+  test("GET:403 Responds with a warning when the token cannot be verified", async () => {
+
+    const { body } = await request(app)
+      .get("/api/auth")
+      .set("Authorization", `Bearer invalidToken`)
+      .expect(403)
+
+    expect(body.message).toBe("Forbidden")
+  })
+
+  test("GET:401 Responds with an authorisation warning when valid credentials have not been provided", async () => {
+
+    const { body } = await request(app)
+      .get("/api/auth")
+      .expect(401)
+
+    expect(body.message).toBe("Unauthorised")
+  })
+})
