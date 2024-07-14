928233e5d0bce6c917f427db07156a06d38c0cf9
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 01:54:32 +0100

    Adds JSDoc annotations

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -6,4 +6,43 @@ import { testAuth } from "../controllers/test-auth"
 export const authRouter = Router()
 
 
+/**
+ * @swagger
+ * /api/auth:
+ *  get:
+ *    summary: Test the authentication system
+ *    description: Responds with a success message when authentication is successful. If the token cannot be verified, or valid user credentials have not been provided, the server responds with an error.
+ *    tags: [Authentication]
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Success
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Forbidden"
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Unauthorised"
+ */
 authRouter.get("/auth", verifyToken, testAuth)
