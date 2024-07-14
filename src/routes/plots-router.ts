b84320350d00e4bf0aea2719a839890487369492
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 18:15:52 +0100

    Adds JSDoc annotations

--- src/routes/plots-router.ts
+++ src/routes/plots-router.ts
@@ -11,7 +11,7 @@ export const plotsRouter = Router()
  * /api/plots/{owner_id}:
  *  get:
  *    summary: Retrieve a user's plots
- *    description: Responds with a an array of plot objects. If no plots are found, the server responds with an error Permission is denied when the current user's ID does not match the target owner_id.
+ *    description: Responds with a an array of plot objects. If no plots are found, the server responds with an error. Permission is denied when the current user's ID does not match the target owner_id.
  *    tags: [Plots]
  *    parameters:
  *      - in: path
@@ -57,16 +57,6 @@ export const plotsRouter = Router()
  *                      area:
  *                        type: integer
  *                        example: 100
- *      400:
- *        description: Bad request
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                message:
- *                  type: string
- *                  example: "Invalid query"
  *      403:
  *        description: Forbidden
  *        content:
@@ -77,8 +67,85 @@ export const plotsRouter = Router()
  *                message:
  *                  type: string
  *                  example: "Access to plot data denied"
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "No results found"
  */
 plotsRouter.get("/plots/:owner_id", verifyToken, getPlotsByOwner)
 
-
+/**
+ * @swagger
+ * /api/plots/{owner_id}/{plot_id}:
+ *  get:
+ *    summary: Retrieve a user's plot
+ *    description: Responds with a single plot object. If no plot is found, the server responds with an error. Permission is denied when the current user's ID does not match the target owner_id.
+ *    tags: [Plots]
+ *    parameters:
+ *      - in: path
+ *        name: owner_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *      - in: path
+ *        name: plot_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                plot_id:
+ *                  type: integer
+ *                  example: 1
+ *                owner_id:
+ *                  type: integer
+ *                  example: 1
+ *                name:
+ *                  type: string
+ *                  example: John's Garden
+ *                type:
+ *                  type: string
+ *                  example: garden
+ *                description:
+ *                  type: string
+ *                  example: A vegetable garden
+ *                location:
+ *                  type: integer
+ *                  example: 1
+ *                area:
+ *                  type: integer
+ *                  example: 100
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Access to plot data denied"
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Plot not found"
+ */
 plotsRouter.get("/plots/:owner_id/:plot_id", verifyToken, getPlotByPlotId)
