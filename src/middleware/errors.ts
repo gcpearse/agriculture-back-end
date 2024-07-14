e846ecf145da81a537d04496903f3121887968fd
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 12:09:26 +0100

    Adds middleware to handle PSQL errors

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -11,7 +11,18 @@ export const handleCustomErrors: ErrorRequestHandler = (err, _req, res, next) =>
 }
 
 
-export const handleServerErrors: ErrorRequestHandler = (_err, _req, res, _next) => {
+export const handlePsqlErrors: ErrorRequestHandler = (err, _req, res, next) => {
 
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Bad request" })
+  } else {
+    next(err)
+  }
+}
+
+
+export const handleServerErrors: ErrorRequestHandler = (err, _req, res, _next) => {
+
+  console.log(err.code)
   res.status(500).send({ message: "Server error" })
 }
