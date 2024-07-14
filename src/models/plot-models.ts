c3155f40a7ef66c4cd6c861d6d55a3051991ead2
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 15:50:17 +0100

    Expands query to optionally filter results by type

--- src/models/plot-models.ts
+++ src/models/plot-models.ts
@@ -1,7 +1,9 @@
+import QueryString from "qs"
 import { db } from "../db"
+import format from "pg-format"
 
 
-export const selectPlotsByOwner = async (authUserId: number, owner_id: number): Promise<any> => {
+export const selectPlotsByOwner = async (authUserId: number, owner_id: number, { type }: QueryString.ParsedQs): Promise<any> => {
 
   if (authUserId !== owner_id) {
     return Promise.reject({
@@ -10,11 +12,16 @@ export const selectPlotsByOwner = async (authUserId: number, owner_id: number): 
     })
   }
 
-  const result = await db.query(`
-    SELECT * FROM plots
-    WHERE owner_id = $1;
-    `,
-    [owner_id])
+  let query = `
+  SELECT * FROM plots
+  WHERE owner_id = $1
+  `
+
+  if (type) {
+    query += format(`AND type = %L`, type)
+  }
+
+  const result = await db.query(query, [owner_id])
 
   if (!result.rowCount) {
     return Promise.reject({
