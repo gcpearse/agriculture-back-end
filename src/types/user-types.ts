2aa85b997cae1b281a3b2da231f4a7dd7f468386
gcpearse <glen.c.pearse@gmail.com>
2024-07-14 13:26:20 +0100

    Reorganises user types

--- src/types/user-types.ts
+++ src/types/user-types.ts
@@ -1,7 +1,12 @@
-export type Credentials = {
+export type Username = {
   username: string
+}
+
+export type Password = {
   password: string
 }
+
+export type Credentials = Username & Password
 
 
 export enum UnitSystem {
@@ -10,9 +15,11 @@ export enum UnitSystem {
 }
 
 
-export type User = {
+export type SecureUser = {
   user_id?: number
   first_name: string
   surname: string
   unit_preference: UnitSystem
-} & Credentials
+} & Username
+
+export type User = SecureUser & Password
