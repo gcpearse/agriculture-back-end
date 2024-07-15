84008716f87d8b224154f1e8d606c7c3f839abc6
gcpearse <glen.c.pearse@gmail.com>
2024-07-15 12:52:01 +0100

    Adds util to prevent user's from creating duplicate plot names

--- src/utils/db-query-utils.ts
+++ src/utils/db-query-utils.ts
@@ -3,12 +3,33 @@ import { db } from "../db"
 
 export const getValidPlotTypes = async (owner_id: number): Promise<string[]> => {
 
-  const validTypes = await db.query(`
+  const result = await db.query(`
     SELECT DISTINCT type
     FROM plots
     WHERE owner_id = $1
     `,
     [owner_id])
 
-  return validTypes.rows.map(row => row.type)
+  return result.rows.map(row => row.type)
+}
+
+
+export const checkPlotNameConflict = async (owner_id: number, name: string) => {
+
+  const result = await db.query(`
+    SELECT name
+    FROM plots
+    WHERE owner_id = $1
+    `,
+    [owner_id])
+
+  const isConflict = result.rows.map(row => row.name).includes(name)
+
+  if (isConflict) {
+    return Promise.reject({
+      status: 409,
+      message: "Conflict",
+      details: "Plot name already exists"
+    })
+  }
 }
