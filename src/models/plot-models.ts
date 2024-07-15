4bdfa0d2641eb3bfc89ee648c41022981210b846
gcpearse <glen.c.pearse@gmail.com>
2024-07-15 17:35:05 +0100

    Adds updatePlotByPlotId model

--- src/models/plot-models.ts
+++ src/models/plot-models.ts
@@ -2,7 +2,7 @@ import QueryString from "qs"
 import { db } from "../db"
 import format from "pg-format"
 import { Plot } from "../types/plot-types"
-import { checkPlotNameConflict, getValidPlotTypes } from "../utils/db-query-utils"
+import { checkPlotNameConflict, getValidPlotTypes, verifyPlotOwner } from "../utils/db-query-utils"
 import { verifyPermission } from "../utils/verification-utils"
 
 
@@ -35,7 +35,7 @@ export const selectPlotsByOwner = async (authUserId: number, owner_id: number, {
 }
 
 
-export const insertPlotByOwner = async (authUserId: number, owner_id: number, plot: Plot) => {
+export const insertPlotByOwner = async (authUserId: number, owner_id: number, plot: Plot): Promise<Plot> => {
 
   await verifyPermission(authUserId, owner_id, "Permission to create plot denied")
 
@@ -54,24 +54,43 @@ export const insertPlotByOwner = async (authUserId: number, owner_id: number, pl
 }
 
 
-export const selectPlotByPlotId = async (authUserId: number, owner_id: number, plot_id: number) => {
+export const selectPlotByPlotId = async (authUserId: number, owner_id: number, plot_id: number): Promise<Plot> => {
 
   await verifyPermission(authUserId, owner_id, "Permission to view plot data denied")
 
-  const result = await db.query(`
-    SELECT * FROM plots
-    WHERE plot_id = $1
-    AND owner_id = $2;
-    `,
-    [plot_id, owner_id])
+  return verifyPlotOwner(plot_id, owner_id, "Permission to view plot data denied")
+}
 
-  if (!result.rowCount) {
-    return Promise.reject({
-      status: 403,
-      message: "Forbidden",
-      details: "Permission to view plot data denied"
-    })
+
+export const updatePlotByPlotId = async (authUserId: number, owner_id: number, plot_id: number, plot: Plot): Promise<Plot> => {
+
+  await verifyPermission(authUserId, owner_id, "Permission to edit plot data denied")
+
+  await verifyPlotOwner(plot_id, owner_id, "Permission to edit plot data denied")
+
+  const currentPlotName = await db.query(`
+    SELECT name
+    FROM plots
+    WHERE plot_id = $1
+    `,
+    [plot_id])
+
+  if (currentPlotName.rows[0].name !== plot.name) {
+    await checkPlotNameConflict(owner_id, plot.name)
   }
+
+  const result = await db.query(`
+    UPDATE plots
+    SET
+      name = $1,
+      type = $2,
+      description = $3,
+      location = $4,
+      area = $5
+    WHERE plot_id = $6
+    RETURNING *;
+    `,
+    [plot.name, plot.type.toLowerCase(), plot.description, plot.location, plot.area, plot_id])
 
   return result.rows[0]
 }
