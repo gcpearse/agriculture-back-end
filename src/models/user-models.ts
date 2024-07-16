d4b2e64ffa2076b0760c401b15b737066f50c792
gcpearse <glen.c.pearse@gmail.com>
2024-07-16 18:15:45 +0100

    Updates models to reflect new email property on user objects

--- src/models/user-models.ts
+++ src/models/user-models.ts
@@ -1,5 +1,6 @@
 import { db } from "../db"
 import { SecureUser } from "../types/user-types"
+import { checkEmailConflict } from "../utils/db-query-utils"
 import { verifyPermission, verifyResult } from "../utils/verification-utils"
 
 
@@ -11,6 +12,7 @@ export const selectUserByUsername = async (authUsername: string, username: strin
     SELECT 
       user_id, 
       username, 
+      email,
       first_name, 
       surname, 
       unit_preference
@@ -29,21 +31,25 @@ export const updateUserByUsername = async (authUsername: string, username: strin
 
   await verifyPermission(authUsername, username, "Permission to edit user data denied")
 
+  await checkEmailConflict(user.email)
+
   const result = await db.query(`
     UPDATE users
     SET 
-      first_name = $1, 
-      surname = $2, 
-      unit_preference = $3
-    WHERE username = $4
+      email = $1,
+      first_name = $2, 
+      surname = $3, 
+      unit_preference = $4
+    WHERE username = $5
     RETURNING 
       user_id, 
       username, 
+      email,
       first_name, 
       surname, 
       unit_preference;
     `,
-    [user.first_name, user.surname, user.unit_preference, username])
+    [user.email, user.first_name, user.surname, user.unit_preference, username])
 
   await verifyResult(!result.rowCount, "User not found")
 
@@ -77,6 +83,7 @@ export const changePasswordByUsername = async (authUsername: string, username: s
     RETURNING 
       user_id, 
       username, 
+      email,
       first_name, 
       surname, 
       unit_preference;
