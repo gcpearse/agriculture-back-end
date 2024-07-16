905d8ddf42839597252ed3ed8491fb3059901f7a
gcpearse <glen.c.pearse@gmail.com>
2024-07-16 17:44:34 +0100

    Updates test cases to handle new email property on user

--- src/__tests__/register.test.ts
+++ src/__tests__/register.test.ts
@@ -17,6 +17,7 @@ describe("POST /api/register", () => {
     const newUser = {
       username: "farmer123",
       password: "password123",
+      email: "fred.flint@example.com",
       first_name: "Fred",
       surname: "Flint",
       unit_preference: "metric"
@@ -30,6 +31,7 @@ describe("POST /api/register", () => {
     expect(body.user).toMatchObject({
       user_id: 4,
       username: "farmer123",
+      email: "fred.flint@example.com",
       first_name: "Fred",
       surname: "Flint",
       unit_preference: "metric"
@@ -41,6 +43,7 @@ describe("POST /api/register", () => {
     const newUser = {
       username: "carrot_king",
       password: "password234",
+      email: "bob.booth@example.com",
       first_name: "Bob",
       surname: "Booth",
       unit_preference: "imperial"
@@ -54,6 +57,28 @@ describe("POST /api/register", () => {
     expect(body).toMatchObject({
       message: "Conflict",
       details: "Username already exists"
+    })
+  })
+
+  test("POST:409 Responds with an error message when the email already exists", async () => {
+
+    const newUser = {
+      username: "farmer123",
+      password: "password123",
+      email: "john.smith@example.com",
+      first_name: "Fred",
+      surname: "Flint",
+      unit_preference: "metric"
+    }
+
+    const { body } = await request(app)
+      .post("/api/register")
+      .send(newUser)
+      .expect(409)
+
+    expect(body).toMatchObject({
+      message: "Conflict",
+      details: "Email already exists"
     })
   })
 })
