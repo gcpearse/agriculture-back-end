9b1e624677c03edbe30e41572ff93aacd1265fe4
gcpearse <glen.c.pearse@gmail.com>
2024-07-16 21:16:54 +0100

    Adds PlotType enum

--- src/types/plot-types.ts
+++ src/types/plot-types.ts
@@ -1,8 +1,20 @@
+export enum PlotType {
+  Allotment = "allotment",
+  Field = "field",
+  Flowerbed = "flowerbed",
+  Garden = "garden",
+  HerbGarden = "herb garden",
+  Homestead = "homestead",
+  Orchard = "orchard",
+  VegetablePatch = "vegetable patch"
+}
+
+
 export type Plot = {
   plot_id?: number
   owner_id: number
   name: string
-  type: string
+  type: PlotType
   description: string
   location: string
   area: number | null
