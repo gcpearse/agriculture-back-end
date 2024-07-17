d409714d6f6f6229693a04e4483f49442ca4c5b2
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 17:32:24 +0100

    Adds check to return 403 when the user tries to access another user's data

--- src/models/subdivision-models.ts
+++ src/models/subdivision-models.ts
@@ -1,9 +1,13 @@
 import { db } from "../db"
+import { getPlotOwnerId } from "../utils/db-query-utils"
+import { verifyPermission } from "../utils/verification-utils"
 
 
 export const selectSubdivisionsByPlotId = async (authUserId: number, plot_id: number) => {
 
-  console.log(authUserId, plot_id)
+  const owner_id = await getPlotOwnerId(plot_id)
+
+  await verifyPermission(authUserId, owner_id, "Permission to view plot subdivision data denied")
 
   const result = await db.query(`
     SELECT * FROM subdivisions
