ef9b1c271590224f65582e3de2a259e36f64373d
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 18:08:00 +0100

    Adds validateSubdivisionType util, reorganises file

--- src/utils/db-query-utils.ts
+++ src/utils/db-query-utils.ts
@@ -1,6 +1,65 @@
 import { db } from "../db"
 
 
+export const checkEmailConflict = async (email: string): Promise<undefined> => {
+
+  const dbEmail = await db.query(`
+    SELECT email 
+    FROM users 
+    WHERE email = $1;
+    `,
+    [email])
+
+  if (dbEmail.rowCount) {
+    return Promise.reject({
+      status: 409,
+      message: "Conflict",
+      details: "Email already exists"
+    })
+  }
+}
+
+
+export const checkPlotNameConflict = async (owner_id: number, name: string): Promise<undefined> => {
+
+  const result = await db.query(`
+    SELECT name
+    FROM plots
+    WHERE owner_id = $1;
+    `,
+    [owner_id])
+
+  if (result.rows.map(row => row.name).includes(name)) {
+    return Promise.reject({
+      status: 409,
+      message: "Conflict",
+      details: "Plot name already exists"
+    })
+  }
+}
+
+
+export const getPlotOwnerId = async (plot_id: number): Promise<number> => {
+
+  const result = await db.query(`
+    SELECT owner_id
+    FROM plots
+    WHERE plot_id = $1;
+    `,
+    [plot_id])
+
+  if (!result.rowCount) {
+    return Promise.reject({
+      status: 404,
+      message: "Not Found",
+      details: "Plot not found"
+    })
+  }
+
+  return result.rows[0].owner_id
+}
+
+
 export const searchForUserId = async (owner_id: number) => {
 
   const result = await db.query(`
@@ -35,25 +94,6 @@ export const searchForUsername = async (username: string) => {
 }
 
 
-export const checkPlotNameConflict = async (owner_id: number, name: string): Promise<undefined> => {
-
-  const result = await db.query(`
-    SELECT name
-    FROM plots
-    WHERE owner_id = $1;
-    `,
-    [owner_id])
-
-  if (result.rows.map(row => row.name).includes(name)) {
-    return Promise.reject({
-      status: 409,
-      message: "Conflict",
-      details: "Plot name already exists"
-    })
-  }
-}
-
-
 export const validatePlotType = async (type: string): Promise<boolean> => {
 
   const result = await db.query(`
@@ -65,41 +105,12 @@ export const validatePlotType = async (type: string): Promise<boolean> => {
 }
 
 
-export const getPlotOwnerId = async (plot_id: number): Promise<number> => {
+export const validateSubdivisionType = async (type: string): Promise<boolean> => {
 
   const result = await db.query(`
-    SELECT owner_id
-    FROM plots
-    WHERE plot_id = $1;
-    `,
-    [plot_id])
+    SELECT type 
+    FROM subdivision_types;
+    `)
 
-  if (!result.rowCount) {
-    return Promise.reject({
-      status: 404,
-      message: "Not Found",
-      details: "Plot not found"
-    })
-  }
-
-  return result.rows[0].owner_id
-}
-
-
-export const checkEmailConflict = async (email: string): Promise<undefined> => {
-
-  const dbEmail = await db.query(`
-    SELECT email 
-    FROM users 
-    WHERE email = $1;
-    `,
-    [email])
-
-  if (dbEmail.rowCount) {
-    return Promise.reject({
-      status: 409,
-      message: "Conflict",
-      details: "Email already exists"
-    })
-  }
+  return result.rows.map(row => row.type).includes(type as string)
 }
