3b7ad8f64115d0ccf6f0da605423e17dffe0f5c5
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 18:09:00 +0100

    Updates model to handle queries

--- src/models/subdivision-models.ts
+++ src/models/subdivision-models.ts
@@ -1,9 +1,12 @@
+import QueryString from "qs"
 import { db } from "../db"
-import { getPlotOwnerId } from "../utils/db-query-utils"
+import { getPlotOwnerId, validateSubdivisionType } from "../utils/db-query-utils"
 import { verifyPermission } from "../utils/verification-utils"
+import { Subdivision } from "../types/subdivision-types"
+import format from "pg-format"
 
 
-export const selectSubdivisionsByPlotId = async (authUserId: number, plot_id: number) => {
+export const selectSubdivisionsByPlotId = async (authUserId: number, plot_id: number, { type }: QueryString.ParsedQs): Promise<Subdivision[]> => {
 
   if (isNaN(plot_id)) {
     return Promise.reject({
@@ -17,11 +20,24 @@ export const selectSubdivisionsByPlotId = async (authUserId: number, plot_id: nu
 
   await verifyPermission(authUserId, owner_id, "Permission to view plot subdivision data denied")
 
-  const result = await db.query(`
-    SELECT * FROM subdivisions
-    WHERE plot_id = $1
-    `,
-    [plot_id])
+  let query = `
+  SELECT * FROM subdivisions
+  WHERE plot_id = $1
+  `
+
+  const isValidSubdivisionType = await validateSubdivisionType(type as string)
+
+  if (type && !isValidSubdivisionType) {
+    return Promise.reject({
+      status: 404,
+      message: "Not Found",
+      details: "No results found for that query"
+    })
+  }
+
+  if (type) query += format(`AND type = %L`, type)
+
+  const result = await db.query(query, [plot_id])
 
   return result.rows
 }
