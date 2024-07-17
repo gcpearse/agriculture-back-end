016e06ddeb66d9fb5f24e14dafeb28f0a02eb461
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 19:24:50 +0100

    Builds main functionality and error handling for insertSubdivisionByPlotId

--- src/models/subdivision-models.ts
+++ src/models/subdivision-models.ts
@@ -35,9 +35,50 @@ export const selectSubdivisionsByPlotId = async (authUserId: number, plot_id: nu
     })
   }
 
-  if (type) query += format(`AND type = %L`, type)
+  if (type) query += format(`AND type = %L;`, type)
 
   const result = await db.query(query, [plot_id])
 
   return result.rows
+}
+
+
+export const insertSubdivisionByPlotId = async (authUserId: number, plot_id: number, subdivision: Subdivision) => {
+
+  if (isNaN(plot_id)) {
+    return Promise.reject({
+      status: 404,
+      message: "Not Found",
+      details: "Plot not found"
+    })
+  }
+
+  let owner_id = await getPlotOwnerId(plot_id)
+
+  await verifyPermission(authUserId, owner_id, "Permission to create subdivision denied")
+
+  owner_id = await getPlotOwnerId(subdivision.plot_id)
+
+  await verifyPermission(authUserId, owner_id, "Permission to create subdivision denied")
+
+  const isValidSubdivisionType = await validateSubdivisionType(subdivision.type as string)
+
+  if (!isValidSubdivisionType) {
+    return Promise.reject({
+      status: 400,
+      message: "Bad Request",
+      details: "Invalid subdivision type"
+    })
+  }
+
+  const result = await db.query(`
+    INSERT INTO subdivisions
+      (plot_id, name, type, description, area)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `,
+    [subdivision.plot_id, subdivision.name, subdivision.type, subdivision.description, subdivision.area])
+
+  return result.rows[0]
 }
