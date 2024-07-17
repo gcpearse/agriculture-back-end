9ae84850e40f6d803e6407d2016fd96487df7d70
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 22:48:51 +0100

    Adds getSubdivisionBySubdivisionId controller

--- src/controllers/subdivision-controllers.ts
+++ src/controllers/subdivision-controllers.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express"
-import { insertSubdivisionByPlotId, selectSubdivisionsByPlotId } from "../models/subdivision-models"
+import { insertSubdivisionByPlotId, selectSubdivisionBySubdivisionId, selectSubdivisionsByPlotId } from "../models/subdivision-models"
 
 
 export const getSubdivisionsByPlotId: RequestHandler = async (req, res, next) => {
@@ -28,6 +28,21 @@ export const postSubdivisionByPlotId: RequestHandler = async (req, res, next) =>
   try {
     const subdivision = await insertSubdivisionByPlotId(authUserId, +plot_id, req.body)
     res.status(201).send({ subdivision })
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+export const getSubdivisionBySubdivisionId: RequestHandler = async (req, res, next) => {
+
+  const authUserId: number = req.body.user.user_id
+
+  const { subdivision_id } = req.params
+
+  try {
+    const subdivision = await selectSubdivisionBySubdivisionId(authUserId, +subdivision_id)
+    res.status(200).send({ subdivision })
   } catch (err) {
     next(err)
   }
