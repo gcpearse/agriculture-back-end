584d4895376f2d4a56bae78db1121d238c981244
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 14:11:12 +0100

    Adds subdivision_id property

--- src/db/data/test-data/issues/issues.ts
+++ src/db/data/test-data/issues/issues.ts
@@ -4,18 +4,28 @@ import { Issue } from "../../../../types/issue-types";
 export default [
   {
     plot_id: 1,
+    subdivision_id: null,
     title: "Broken gate",
     description: "The gate has fallen off its hinges",
     is_resolved: false
   },
   {
     plot_id: 1,
+    subdivision_id: null,
     title: "Weeds",
     description: "The garden has become infested with weeds",
     is_resolved: true
   },
   {
+    plot_id: 1,
+    subdivision_id: 1,
+    title: "Slug infestation",
+    description: "Slugs are destroying the carrots",
+    is_resolved: false
+  },
+  {
     plot_id: 2,
+    subdivision_id: null,
     title: "Rotten fence posts",
     description: "Some of the fence posts are rotting and need replacing",
     is_resolved: false
