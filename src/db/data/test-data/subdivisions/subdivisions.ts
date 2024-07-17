07b9aaef46b91825191568f4cae87b6b0317c015
gcpearse <glen.c.pearse@gmail.com>
2024-07-17 12:42:43 +0100

    Adds test data

---
+++ src/db/data/test-data/subdivisions/subdivisions.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    plot_id: 1,
+    name: "Root Vegetable Bed",
+    type: "bed",
+    description: "Carrots, beetroots, and parsnips",
+    area: 10
+  },
+  {
+    plot_id: 1,
+    name: "Onion Bed",
+    type: "bed",
+    description: "Red onions, garlic, and shallots",
+    area: 10
+  },
+  {
+    plot_id: 1,
+    name: "Woody Herbs",
+    type: "herb garden",
+    description: "Rosemary and thyme",
+    area: 2
+  },
+  {
+    plot_id: 2,
+    name: "Cooking Apples",
+    type: "row",
+    description: "Bramley and Lord Derby apple trees",
+    area: 20
+  }
+]
