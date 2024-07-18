2c874649cc1850eee1eb2b6541530dcab96ff6c2
gcpearse <glen.c.pearse@gmail.com>
2024-07-18 01:05:17 +0100

    Adds data set

---
+++ src/db/data/test-data/subdivisions/subdivision-images.ts
@@ -0,0 +1,13 @@
+import { SubdivisionImage } from "../../../../types/image-types";
+
+
+export const subdivisionImageData: SubdivisionImage[] = [
+  {
+    subdivision_id: 1,
+    image_url: "https://images.pexels.com/photos/169523/pexels-photo-169523.jpeg?auto=compress&cs=tinysrgb&w=600"
+  },
+  {
+    subdivision_id: 3,
+    image_url: "https://images.pexels.com/photos/3912947/pexels-photo-3912947.jpeg?auto=compress&cs=tinysrgb&w=600"
+  }
+]
