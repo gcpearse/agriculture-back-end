fcd4320905d3f1a51695e06d411956797efee6a7
gcpearse <glen.c.pearse@gmail.com>
2024-07-20 15:16:16 +0100

    Adds getCropsByPlotId controller

---
+++ src/controllers/crop-controllers.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express"
+import { selectCropsByPlotId } from "../models/crop-models"
+
+
+export const getCropsByPlotId: RequestHandler = async (req, res, next) => {
+
+  const authUserId: number = req.body.user.user_id
+
+  const { plot_id } = req.params
+
+  try {
+    const crops = await selectCropsByPlotId(authUserId, +plot_id)
+    res.status(200).send({ crops })
+  } catch (err) {
+    next(err)
+  }
+}
