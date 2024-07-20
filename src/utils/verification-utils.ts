e0c3e2dda9283758a6b6ce9e18f357b02bde8984
gcpearse <glen.c.pearse@gmail.com>
2024-07-20 21:57:49 +0100

    ->  with additional checks for positive integers

--- src/utils/verification-utils.ts
+++ src/utils/verification-utils.ts
@@ -10,9 +10,9 @@ export const verifyPermission = (base: string | number, target: string | number,
 }
 
 
-export const verifyParamIsNumber = (param: number): Promise<never> | undefined => {
+export const verifyParamIsPositiveInt = (param: number): Promise<never> | undefined => {
 
-  if (isNaN(param)) {
+  if (isNaN(param) || param < 1 || Math.floor(param) !== param) {
     return Promise.reject({
       status: 400,
       message: "Bad Request",
