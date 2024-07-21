bcd8d58fd608421f23c89b9af6de3b7a05f0279e
gcpearse <glen.c.pearse@gmail.com>
2024-07-21 23:54:45 +0100

    Adds new route for posting crop by plot_id with JSDoc annotations

--- src/routes/crops-router.ts
+++ src/routes/crops-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { verifyToken } from "../middleware/authentication"
-import { getCropsByPlotId } from "../controllers/crop-controllers"
+import { getCropsByPlotId, postCropByPlotId } from "../controllers/crop-controllers"
 
 
 export const cropsRouter = Router()
@@ -56,32 +56,7 @@ cropsRouter.route("/crops/plot/:plot_id")
  *            schema:
  *              type: array
  *              items:
- *                type: object
- *                properties:
- *                  crop_id:
- *                    type: integer
- *                    example: 1
- *                  plot_id:
- *                    type: integer
- *                    example: 1
- *                  subdivision_id:
- *                    type: integer
- *                    example: 1
- *                  name:
- *                    type: string
- *                    example: carrot
- *                  variety:
- *                    type: string
- *                    example: chantenay
- *                  quantity:
- *                    type: integer
- *                    example: 20
- *                  date_planted:
- *                    type: string
- *                    example: 2024-06-19T23:00:00.000Z
- *                  harvest_date:
- *                    type: string
- *                    example: 2024-09-14T23:00:00.000Z
+ *                $ref: "#/components/schemas/Crop"
  *      400:
  *        description: Bad Request
  *        content:
@@ -102,3 +77,80 @@ cropsRouter.route("/crops/plot/:plot_id")
  *              $ref: "#/components/schemas/NotFound"
  */
   .get(verifyToken, getCropsByPlotId)
+
+
+/**
+ * @swagger
+ * /api/crops/plot/{plot_id}:
+ *  post:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Add a new crop to a plot
+ *    description: Responds with a crop object. If the plot_id does not exist, the server responds with an error. Permission is denied when the plot does not belong to the current user.
+ *    tags: [Crops]
+ *    parameters:
+ *      - in: path
+ *        name: plot_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              plot_id:
+ *                type: integer
+ *                example: 1
+ *              subdivision_id:
+ *                type: integer
+ *                nullable: true
+ *                example: null
+ *              name:
+ *                type: string
+ *                example: Root Vegetable Bed
+ *              variety:
+ *                type: string
+ *                nullable: true
+ *                example: bed
+ *              quantity:
+ *                type: integer
+ *                nullable: true
+ *                example: 1
+ *              date_planted:
+ *                type: string
+ *                nullable: true
+ *                example: 2024-07-21
+ *              harvest_date:
+ *                type: string
+ *                nullable: true
+ *                example: 2024-09-30
+ *    responses:
+ *      201:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Crop"
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/BadRequest"
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Forbidden"
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/NotFound"
+ */
+  .post(verifyToken, postCropByPlotId)
