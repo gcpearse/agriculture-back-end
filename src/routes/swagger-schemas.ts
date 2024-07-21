96e98e141e70fcb23d373b85bf7443ea9a663d7f
gcpearse <glen.c.pearse@gmail.com>
2024-07-21 23:54:15 +0100

    Adds Crop to component schemas

--- src/routes/swagger-schemas.ts
+++ src/routes/swagger-schemas.ts
@@ -26,6 +26,34 @@ export const schemaRouter = Router()
  *        details:
  *          type: string
  *          example: Content already exists
+ *    Crop:
+ *      type: object
+ *      properties:
+ *        crop_id:
+ *          type: integer
+ *          example: 1
+ *        plot_id:
+ *          type: integer
+ *          example: 1
+ *        subdivision_id:
+ *          type: integer
+ *          nullable: true
+ *          example: null
+ *        name:
+ *          type: string
+ *          example: carrot
+ *        variety:
+ *          type: string
+ *          example: chantenay
+ *        quantity:
+ *          type: integer
+ *          example: 20
+ *        date_planted:
+ *          type: string
+ *          example: 2024-06-19T23:00:00.000Z
+ *        harvest_date:
+ *          type: string
+ *          example: 2024-09-14T23:00:00.000Z
  *    Forbidden:
  *      type: object
  *      properties:
