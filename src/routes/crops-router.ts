3f0b570b1bb8d1dd4c3f9fb73b7f6e486f1c3570
gcpearse <glen.c.pearse@gmail.com>
2024-07-22 19:24:34 +0100

    Adds new router and route to get from /api/crops/subdivision/:subdivision_id with JSDoc annotations

--- src/routes/crops-router.ts
+++ src/routes/crops-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { verifyToken } from "../middleware/authentication"
-import { getCropsByPlotId, postCropByPlotId } from "../controllers/crop-controllers"
+import { getCropsByPlotId, getCropsBySubdivisionId, postCropByPlotId } from "../controllers/crop-controllers"
 
 
 export const cropsRouter = Router()
@@ -163,3 +163,115 @@ cropsRouter.route("/crops/plot/:plot_id")
  *              $ref: "#/components/schemas/NotFound"
  */
   .post(verifyToken, postCropByPlotId)
+
+
+cropsRouter.route("/crops/subdivision/:subdivision_id")
+
+
+/**
+ * @swagger
+ * /api/crops/subdivision/{subdivision_id}:
+ *  get:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Retrieve a subdivision's crops
+ *    description: Responds with an array of crop objects. If a query parameter is invalid or the subdivision_id does not exist, the server responds with an error. Permission is denied when the subdivision does not belong to the current user.
+ *    tags: [Crops]
+ *    parameters:
+ *      - in: path
+ *        name: subdivision_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *      - in: query
+ *        name: name
+ *        schema:
+ *          type: string
+ *      - in: query
+ *        name: sort
+ *        schema:
+ *          type: string
+ *        default: crop_id
+ *      - in: query
+ *        name: order
+ *        schema:
+ *          type: string
+ *        default: asc
+ *      - in: query
+ *        name: limit
+ *        schema:
+ *          type: integer
+ *        default: 10
+ *      - in: query
+ *        name: page
+ *        schema:
+ *          type: integer
+ *        default: 1
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                crops:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      crop_id:
+ *                        type: integer
+ *                        example: 1
+ *                      plot_id:
+ *                        type: integer
+ *                        example: 1
+ *                      subdivision_id:
+ *                        type: integer
+ *                        nullable: true
+ *                        example: null
+ *                      name:
+ *                        type: string
+ *                        example: carrot
+ *                      variety:
+ *                        type: string
+ *                        nullable: true
+ *                        example: chantenay
+ *                      quantity:
+ *                        type: integer
+ *                        nullable: true
+ *                        example: 20
+ *                      date_planted:
+ *                        type: string
+ *                        nullable: true
+ *                        example: 2024-06-19T23:00:00.000Z
+ *                      harvest_date:
+ *                        type: string
+ *                        nullable: true
+ *                        example: 2024-09-14T23:00:00.000Z
+ *                      note_count:
+ *                        type: integer
+ *                        example: 1
+ *                count:
+ *                  type: integer
+ *                  example: 1
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/BadRequest"
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Forbidden"
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/NotFound"
+ */
+  .get(verifyToken, getCropsBySubdivisionId)
