5cf79800b53756876d4a1f4a62dfa02349db67fe
gcpearse <glen.c.pearse@gmail.com>
2024-07-27 18:17:14 +0100

    Adds cropCategoryData to SeedData type

--- src/types/seed-types.ts
+++ src/types/seed-types.ts
@@ -9,19 +9,20 @@ import { User } from "./user-types"
 
 
 export type SeedData = {
-  userData: User[],
-  plotData: Plot[],
-  plotImageData: PlotImage[],
-  plotTypeData: { type: string }[],
-  subdivisionData: Subdivision[],
-  subdivisionImageData: SubdivisionImage[],
-  subdivisionTypeData: { type: string }[],
-  cropData: Crop[],
-  cropNoteData: CropNote[],
-  cropImageData: CropImage[],
-  issueData: Issue[],
-  issueNoteData: IssueNote[],
-  issueImageData: IssueImage[],
-  jobData: Job[],
+  userData: User[]
+  plotData: Plot[]
+  plotImageData: PlotImage[]
+  plotTypeData: { type: string }[]
+  subdivisionData: Subdivision[]
+  subdivisionImageData: SubdivisionImage[]
+  subdivisionTypeData: { type: string }[]
+  cropData: Crop[]
+  cropNoteData: CropNote[]
+  cropImageData: CropImage[]
+  cropCategoryData: { category: string }[]
+  issueData: Issue[]
+  issueNoteData: IssueNote[]
+  issueImageData: IssueImage[]
+  jobData: Job[]
   jobImageData: JobImage[]
 }
