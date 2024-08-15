e09d51486418dd3cfcbbc39be61fd9509b9e6cfc
gcpearse <glen.c.pearse@gmail.com>
2024-08-15 14:30:27 +0100

    Adds middleware test suite

---
+++ src/__tests__/middleware.test.ts
@@ -0,0 +1,27 @@
+import data from "../db/data/test-data/test-index"
+import { db } from "../db"
+import { seed } from "../db/seeding/seed"
+import request from "supertest"
+import { app } from "../app"
+import { StatusResponse } from "../types/response-types"
+
+
+beforeEach(() => seed(data))
+
+afterAll(() => db.end())
+
+
+describe("ANY /api/foobar", () => {
+
+  test("GET:404 Responds with an error when the path is not found", async () => {
+
+    const { body } = await request(app)
+      .get("/api/foobar")
+      .expect(404)
+
+    expect(body).toMatchObject<StatusResponse>({
+      message: "Not Found",
+      details: "Path not found"
+    })
+  })
+})
