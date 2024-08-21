decc33ae63c5d2ad0c7e3995ceb2162a14e98760
gcpearse <glen.c.pearse@gmail.com>
2024-08-21 22:53:28 +0100

    Creates new crop notes router and /api/crop_notes/crop/:crop_id route

---
+++ src/routes/crop-notes-router.ts
@@ -0,0 +1,28 @@
+import { Router } from "express"
+import { verifyToken } from "../middleware/authentication"
+import { getCropNotesByCropId } from "../controllers/crop-notes-controllers"
+
+
+export const cropNotesRouter = Router()
+
+
+cropNotesRouter.route("/crop_notes/crop/:crop_id")
+
+
+/**
+ * @swagger
+ * /api/crop_notes/crop/{crop_id}:
+ *  get:
+ *    security:
+ *      - bearerAuth: []
+ *    summary: Retrieve a crop's notes
+ *    description: Responds with an array of crop note objects.
+ *    tags: [Crop notes]
+ *    parameters:
+ *      - in: path
+ *        name: crop_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ */
+  .get(verifyToken, getCropNotesByCropId)
