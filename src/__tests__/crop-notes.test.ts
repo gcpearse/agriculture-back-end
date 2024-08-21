4d950b4b6d1e5f88e10222026cd5b6b44d8a037f
gcpearse <glen.c.pearse@gmail.com>
2024-08-21 22:54:11 +0100

    Sets up new test suite

---
+++ src/__tests__/crop-notes.test.ts
@@ -0,0 +1,42 @@
+import data from "../db/data/test-data/test-index"
+import { db } from "../db"
+import { seed } from "../db/seeding/seed"
+import request from "supertest"
+import { app } from "../app"
+
+
+let token: string
+
+
+beforeEach(async () => {
+
+  await seed(data)
+
+  const auth = await request(app)
+    .post("/api/login")
+    .send({
+      login: "carrot_king",
+      password: "carrots123",
+    })
+
+  token = auth.body.token
+})
+
+afterAll(async () => {
+  await seed(data)
+  db.end()
+})
+
+
+describe("GET /api/crop_notes/crop/:crop_id", () => {
+
+  test("GET:200 Responds with an array of crop note objects", async () => {
+
+    const { body } = await request(app)
+      .get("/api/crop_notes/crop/1")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+
+    console.log(body.cropNotes)
+  })
+})
