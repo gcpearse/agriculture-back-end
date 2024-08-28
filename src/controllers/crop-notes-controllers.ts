ed7beb8fb571b91ce28d27f0bd2db3a697ebc436
gcpearse <glen.c.pearse@gmail.com>
2024-08-28 18:49:56 +0100

    Adds deleteCropNotesByCropId controller

--- src/controllers/crop-notes-controllers.ts
+++ src/controllers/crop-notes-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response } from "express"
 import { ExtendedRequest } from "../types/auth-types"
-import { insertCropNoteByCropId, selectCropNotesByCropId } from "../models/crop-notes-models"
+import { insertCropNoteByCropId, removeCropNotesByCropId, selectCropNotesByCropId } from "../models/crop-notes-models"
 
 
 export const getCropNotesByCropId = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
@@ -27,6 +27,21 @@ export const postCropNoteByCropId = async (req: ExtendedRequest, res: Response, 
   try {
     const note = await insertCropNoteByCropId(authUserId, +crop_id, req.body)
     res.status(201).send({ note })
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+export const deleteCropNotesByCropId = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+
+  const authUserId = req.user!.user_id
+
+  const { crop_id } = req.params
+
+  try {
+    await removeCropNotesByCropId(authUserId, +crop_id)
+    res.status(204).send()
   } catch (err) {
     next(err)
   }
