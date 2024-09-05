8ec1ae6d26d8d7e341e4de368c765f8e130848b9
gcpearse <glen.c.pearse@gmail.com>
2024-09-05 15:55:08 +0100

    Updates query to return subdivision_name, note_count, and image_count properties on response objects

--- src/models/issues-models.ts
+++ src/models/issues-models.ts
@@ -1,5 +1,5 @@
 import { db } from "../db"
-import { Issue } from "../types/issue-types"
+import { ExtendedIssue } from "../types/issue-types"
 import { fetchPlotOwnerId } from "../utils/db-queries"
 import { verifyPermission, verifyValueIsPositiveInt } from "../utils/verification"
 
@@ -7,7 +7,7 @@ import { verifyPermission, verifyValueIsPositiveInt } from "../utils/verificatio
 export const selectIssuesByPlotId = async (
   authUserId: number,
   plot_id: number
-): Promise<Issue[]> => {
+): Promise<ExtendedIssue[]> => {
 
   await verifyValueIsPositiveInt(plot_id)
 
@@ -16,9 +16,23 @@ export const selectIssuesByPlotId = async (
   await verifyPermission(authUserId, owner_id)
 
   let query = `
-  SELECT *
+  SELECT
+    issues.*,
+    subdivisions.name
+    AS subdivision_name,
+    COUNT(DISTINCT issue_notes.note_id)::INT
+    AS note_count,
+    COUNT(DISTINCT issue_images.image_id)::INT
+    AS image_count
   FROM issues
+  LEFT JOIN subdivisions
+  ON issues.subdivision_id = subdivisions.subdivision_id
+  LEFT JOIN issue_notes
+  ON issues.issue_id = issue_notes.issue_id
+  LEFT JOIN issue_images
+  ON issues.issue_id = issue_images.issue_id
   WHERE issues.plot_id = $1
+  GROUP BY issues.issue_id, subdivisions.name
   ORDER BY issues.issue_id DESC, issues.title;
   `
 
