eb7d451b4df317b9a120712a3e8060d195e3251c
gcpearse <glen.c.pearse@gmail.com>
2024-09-13 12:23:45 +0100

    Updates JSDoc annotations

--- src/routes/issues-router.ts
+++ src/routes/issues-router.ts
@@ -113,7 +113,7 @@ issuesRouter.route("/issues/subdivisions/:subdivision_id")
  *    security:
  *      - bearerAuth: []
  *    summary: Retrieve a subdivision's issues
- *    description: Responds with an array of issue objects.
+ *    description: Responds with an array of issue objects. If a parameter is invalid, the server responds with an error. Permission is denied when the plot does not belong to the user.
  *    tags: [Issues]
  *    parameters:
  *      - in: path
@@ -121,5 +121,44 @@ issuesRouter.route("/issues/subdivisions/:subdivision_id")
  *        required: true
  *        schema:
  *          type: integer
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                issues:
+ *                  type: array
+ *                  items:
+ *                    allOf:
+ *                      - $ref: "#/components/schemas/Issue"
+ *                      - type: object
+ *                        properties:
+ *                          note_count:
+ *                            type: integer
+ *                            example: 1
+ *                          image_count:
+ *                            type: integer
+ *                            example: 1
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/BadRequest"
+ *      403:
+ *        description: Forbidden
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Forbidden"
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/NotFound"
  */
   .get(verifyToken, getIssuesBySubdivisionId)
