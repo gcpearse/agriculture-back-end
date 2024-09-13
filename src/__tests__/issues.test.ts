8769336921cd10300ed3075387fe29eb3c491fb4
gcpearse <glen.c.pearse@gmail.com>
2024-09-13 12:19:30 +0100

    Adds tests for GET /api/issues/subdivisions/:subdivision_id

--- src/__tests__/issues.test.ts
+++ src/__tests__/issues.test.ts
@@ -64,7 +64,7 @@ describe("GET /api/issues/plots/:plot_id", () => {
       })
     }
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:200 Responds with an empty array when no issues are associated with the plot", async () => {
@@ -265,7 +265,7 @@ describe("GET /api/issues/plots/:plot_id?sort=", () => {
 
     expect(body.issues).toEqual(sortedIssues)
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:400 Responds with an error when passed an invalid sort value", async () => {
@@ -337,7 +337,7 @@ describe("GET /api/issues/plots/:plot_id?limit=", () => {
 
     expect(body.issues).toHaveLength(2)
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:200 Responds with an array of all issues associated with the plot when the limit exceeds the total number of results", async () => {
@@ -347,9 +347,9 @@ describe("GET /api/issues/plots/:plot_id?limit=", () => {
       .set("Authorization", `Bearer ${token}`)
       .expect(200)
 
-    expect(body.issues).toHaveLength(3)
+    expect(body.issues).toHaveLength(4)
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:400 Responds with an error when the value of limit is not a positive integer", async () => {
@@ -378,9 +378,9 @@ describe("GET /api/issues/plots/:plot_id?page=", () => {
 
     expect(body.issues.map((issue: ExtendedIssue) => {
       return issue.issue_id
-    })).toEqual([1])
+    })).toEqual([2, 1])
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:200 The page defaults to page one", async () => {
@@ -392,9 +392,9 @@ describe("GET /api/issues/plots/:plot_id?page=", () => {
 
     expect(body.issues.map((issue: ExtendedIssue) => {
       return issue.issue_id
-    })).toEqual([3, 2])
+    })).toEqual([5, 3])
 
-    expect(body.count).toBe(3)
+    expect(body.count).toBe(4)
   })
 
   test("GET:400 Responds with an error when the value of page is not a positive integer", async () => {
@@ -420,6 +420,109 @@ describe("GET /api/issues/plots/:plot_id?page=", () => {
     expect(body).toMatchObject<StatusResponse>({
       message: "Not Found",
       details: "Page not found"
+    })
+  })
+})
+
+
+describe("GET /api/issues/subdivisions/:subdivision_id", () => {
+
+  test("GET:200 Responds with an array of issue objects sorted by issue_id in descending order", async () => {
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/1")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+
+    const sortedIssues: ExtendedIssue[] = [...body.issues].sort((a: ExtendedIssue, b: ExtendedIssue) => {
+      if (a.issue_id! < b.issue_id!) return 1
+      if (a.issue_id! > b.issue_id!) return -1
+      return 0
+    })
+
+    expect(body.issues).toEqual(sortedIssues)
+
+    for (const issue of body.issues) {
+      expect(issue).toMatchObject<ExtendedIssue>({
+        issue_id: expect.any(Number),
+        plot_id: 1,
+        subdivision_id: 1,
+        title: expect.any(String),
+        description: expect.any(String),
+        is_critical: expect.any(Boolean),
+        is_resolved: expect.any(Boolean),
+        note_count: expect.any(Number),
+        image_count: expect.any(Number)
+      })
+    }
+  })
+
+  test("GET:200 Responds with an empty array when no issues are associated with the plot", async () => {
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/2")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(200)
+
+    expect(Array.isArray(body.issues)).toBe(true)
+
+    expect(body.issues).toHaveLength(0)
+  })
+
+  test("GET:400 Responds with an error when the subdivision_id parameter is not a positive integer", async () => {
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/foobar")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(400)
+
+    expect(body).toMatchObject<StatusResponse>({
+      message: "Bad Request",
+      details: "Value must be a positive integer"
+    })
+  })
+
+  test("GET:403 Responds with an error when the authenticated user attempts to retrieve another user's issue data", async () => {
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/4")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(403)
+
+    expect(body).toMatchObject<StatusResponse>({
+      message: "Forbidden",
+      details: "Permission denied"
+    })
+  })
+
+  test("GET:404 Responds with an error when the plot does not exist", async () => {
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/999")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(404)
+
+    expect(body).toMatchObject<StatusResponse>({
+      message: "Not Found",
+      details: "Subdivision not found"
+    })
+  })
+
+  test("GET:404 When the parent subdivision is deleted, all child issues are also deleted", async () => {
+
+    await request(app)
+      .delete("/api/subdivisions/1")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(204)
+
+    const { body } = await request(app)
+      .get("/api/issues/subdivisions/1")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(404)
+
+    expect(body).toMatchObject<StatusResponse>({
+      message: "Not Found",
+      details: "Subdivision not found"
     })
   })
 })
