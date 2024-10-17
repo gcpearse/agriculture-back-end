6af19f5cb8b1b8c7893b4f27d2f8229d415622df
gcpearse <glen.c.pearse@gmail.com>
2024-10-17 23:30:32 +0100

    Adds check to prevent old and new password match from being accepted

--- src/models/users-models.ts
+++ src/models/users-models.ts
@@ -226,6 +226,15 @@ export const updatePasswordByUserId = async (
     })
   }
 
+  // Compare values once old password has been verified
+  if (oldPassword === newPassword) {
+    return Promise.reject({
+      status: 409,
+      message: "Conflict",
+      details: "New password must not match old password"
+    })
+  }
+
   const hashedPassword = await generateHash(newPassword)
 
   await db.query(`
